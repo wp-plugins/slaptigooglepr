c545181e3cc4acff73bf0ce5f7be6b41c2c5b2e9
slaptijack <slaptijack@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-13 21:32:41 +0000

    Fixed up plugin display in plugin admin page
    git-svn-id: https://plugins.svn.wordpress.org/slaptigooglepr/trunk@145640 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- slaptigooglepr.php
+++ slaptigooglepr.php
@@ -1,10 +1,10 @@
 <?php
 /*
-Plugin Name: SlaptiGooglePR
+Plugin Name: Google PageRank Tool
 Plugin URI: http://slaptijack.com/projects/
 Description: This plugin adds a Google PageRank (PR) column to your Manage Pages and Manage Posts administration panels.  This will give you the individual PageRank of each page / post on your site. Also includes site-wide PageRank in the top right hand corner of your WordPress Dashboard.
 Version: 0.4.0
-Author: Scott Hebert (Slaptijack)
+Author: Scott Hebert
 Author URI: http://slaptijack.com
 */
 
